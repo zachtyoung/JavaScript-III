2da14006b465e9d4b845ffd611381c0680550269
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2019-06-12 18:54:25 -0500

    WIP

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,45 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global binding
+* 2. Implicit binding
+* 3. New binding
+* 4. Explicit binding
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
+// console.log(this)
+// // code example for Window Binding
 
-// code example for Window Binding
-
-// Principle 2
-
+// // Principle 2
+// const myObj = {
+//     greeting: "What's up",
+//     sayHi: function (name) {
+//         console.log(`${this.greeting} my name is ${name}`);
+//     }
+// }
+// myObj.sayHi('Zach')
 // code example for Implicit Binding
+// function Person(speaker, favColor){
+//     this.opener = "Good afternoon";
+//     this.speaker = speaker;
+//     this.favColor = favColor;
+//     this.talk = function(){
+//         console.log(`${this.opener} ladies and gentleman, my name is ${this.speaker} and my favorite color is ${this.favColor}`)
+//     }
 
-// Principle 3
+// }
+// const Zach = new Person ('Zach', 'red')
+// const Alex = new Person ('Alex', 'green')
 
-// code example for New Binding
+// // Zach.talk();
+// // Alex.talk();
+// // Principle 3
 
+// // code example for New Binding
+// Zach.talk.call(Alex)
 // Principle 4
 
 // code example for Explicit Binding
